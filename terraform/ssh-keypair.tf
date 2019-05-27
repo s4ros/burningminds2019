cee99431c43c12ee7ebfdd3d7d3096a73a529cc9
Łukasz Wojno <lukasz.wojno@netguru.pl>
2019-05-27 23:00:09 +0200

    Add Terraform ec2 instance template

---
+++ terraform/ssh-keypair.tf
@@ -0,0 +1,4 @@
+resource "aws_key_pair" "deploy" {
+  key_name   = "stefan-ssh-key"
+  public_key = "${file("${path.module}/stefan.burningminds2019")}"
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,15 @@
 variable "aws_region" {
   default = "eu-central-1"
 }
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "project" {
+  default = "burningminds2019"
+}
+
+variable "environment" {
+  default = "production"
+}
